4dd3d1f8e8fec01b328b6eb72446ecdee28e9a00
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-06-11 21:23:56 +0300

    feat: hello world controller params

--- src/main/java/com/hei/project/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/hei/project/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,15 +1,21 @@
 package com.hei.project.endpoint.rest.controller.health;
 
 
+import com.hei.project.service.HelloWorldService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
 
+    private final HelloWorldService helloWorldService;
+
     @GetMapping("/hello")
-    public String hello() {
-        return ".....World";
+    public String hello(@RequestParam String name) {
+        return helloWorldService.uploadHelloWorldMessage(name);
     }
 
 }
