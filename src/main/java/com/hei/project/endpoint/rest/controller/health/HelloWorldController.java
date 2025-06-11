6349de5d2ddf3e9f637363c8f2525edf12d028a1
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-06-11 22:03:02 +0300

    email sending controller

--- src/main/java/com/hei/project/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/hei/project/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,21 +1,31 @@
 package com.hei.project.endpoint.rest.controller.health;
 
 
+import com.hei.project.mail.Email;
+import com.hei.project.mail.Mailer;
 import com.hei.project.service.HelloWorldService;
+import jakarta.mail.internet.InternetAddress;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
 
     private final HelloWorldService helloWorldService;
+    private final Mailer mailer;
 
     @GetMapping("/hello")
-    public String hello(@RequestParam String name) {
-        return helloWorldService.uploadHelloWorldMessage(name);
+    @SneakyThrows
+    public String hello(@RequestParam String to) {
+        var email = new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "hellooooooo .... world!", List.of());
+        mailer.accept(email);
+        return "...world!";
     }
 
 }
